6b6a8ee6304ff67409ccd58e7bae43eb825a78fe
Rizwan Ali <rizali.8116@gmail.com>
2022-08-29 08:55:59 +0500

    Implemented Export

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var rect = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-};
+var rect = require('./rectangle');
 
 function rectangle(length,width){
     console.log("Computing Area and Parameter for Rectangle with length: "+length+" and width: "+width);
